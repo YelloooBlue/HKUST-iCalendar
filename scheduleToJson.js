dc69af136574f93d6438fd2b84149779c79f7624
YelloooBlue <YelloooBlue@outlook.com>
2024-08-31 17:21:28 +0800

    test about split

--- scheduleToJson.js
+++ scheduleToJson.js
@@ -33,7 +33,8 @@ export function extractCoursesJson() {
             var components = classTr.querySelector("span[id^='MTG_COMP$']")?.innerText.trim();
             var daysTimes = classTr.querySelector("span[id^='MTG_SCHED$']")?.innerText.trim();
             var room = classTr.querySelector("span[id^='MTG_LOC$']")?.innerText.trim();
-            var instructor = classTr.querySelector("span[id^='DERIVED_CLS_DTL_SSR_INSTR_LONG$']")?.innerText.trim();
+            var instructor_tmp = classTr.querySelector("span[id^='DERIVED_CLS_DTL_SSR_INSTR_LONG$']")?.innerHTML; //!
+            var instructor = instructor_tmp?.replace(/<br>/g, "\n").replace(/<span.*?>/g, "").replace(/<\/span>/g, "").trim(); //!
             var startEndDate = classTr.querySelector("span[id^='MTG_DATES$']")?.innerText.trim();
 
             var classInfo = {
